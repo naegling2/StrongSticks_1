728d545170ca5091716aefec543669797a19333b
naegling2 <orenslk@yahoo.com>
2014-06-23 17:44:47 -0400

    mcmod.info and src file structure

---
+++ src/main/java/com/naegling2/strongsticks/StrongSticks.java
@@ -0,0 +1,28 @@
+package com.naegling2.strongsticks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="StrongSticks", name="StrongSticks", version="1.7.2-1.0")
+public class StrongSticks {
+
+    @Mod.Instance("StongSticks")
+    public static StrongSticks instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
